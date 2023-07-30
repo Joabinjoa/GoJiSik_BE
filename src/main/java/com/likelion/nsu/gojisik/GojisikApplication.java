5ad53c530df264341ad17083a620ca479637422c
kws9208 <kws9208@gmail.com>
2023-07-31 00:10:12 +0900

    [Chore] 프로젝트 초기 설정

---
+++ src/main/java/com/likelion/nsu/gojisik/GojisikApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.nsu.gojisik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojisikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GojisikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/nsu/gojisik/GojisikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.nsu.gojisik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GojisikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
